7cfc3f99048561d47989eb90683eb78726cdb795
hananisthebest <ademhanan804@gmail.com>
2024-12-03 09:29:24 -0500

    yippeee

--- js.js
+++ js.js
@@ -3,6 +3,7 @@ const headerEl = document.querySelector('.header');
 window.addEventListener('scroll', () => {
     if (window.scrollY > 50) {
         headerEl.classList.add('header-scrolled');
+    } else if (window.scrollY <= 50) {
+        headerEl.classList.remove('header-scrolled'); 
     }
-
 });

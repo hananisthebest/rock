5e9414d3ba954d96622e348e7e3b10e731d20b55
1nick-18 <MM26NSmith@efcts.us>
2024-12-06 02:07:09 -0500

    a lot of stuff

--- js.js
+++ js.js
@@ -9,3 +9,16 @@ window.addEventListener('scroll', () => {
 });  
 
 $('.carousel').carousel() 
+
+window.onscroll = function() {stickyNavbar()};
+
+var navbar = document.querySelector('.header');
+var sticky = navbar.offsetTop;
+
+function stickyNavbar() {
+  if (window.pageYOffset > sticky) {
+    navbar.classList.add('sticky');
+  } else {
+    navbar.classList.remove('sticky');
+  }
+}
